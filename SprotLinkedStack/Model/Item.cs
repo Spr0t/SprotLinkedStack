564126dd40dc52f32b028ac4df8895151f3abe33
Serj Prots <serganas995@gmail.com>
2022-06-02 17:37:24 +0300

    Добавьте файлы проекта.

---
+++ SprotLinkedStack/Model/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprotLinkedStack.Model
+{
+    public class Item<T>
+    {
+        public T Data { get; set; }
+
+        public Item<T> Previous { get; set; }
+
+        public Item(T data)
+        {
+            Data = data;
+        }
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+
+
+    }
+}
---
+++ SprotLinkedStack/Program.cs
@@ -0,0 +1,25 @@
+﻿using SprotLinkedStack.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprotLinkedStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedStack<int> linkedStack = new LinkedStack<int>();
+            linkedStack.Push(10);
+            linkedStack.Push(20);
+
+            Console.WriteLine(linkedStack.Peek());
+            Console.WriteLine(linkedStack.Pop());
+            Console.WriteLine(linkedStack.Pop());
+            Console.WriteLine(linkedStack.Peek());
+            Console.ReadLine();
+        }
+    }
+}
